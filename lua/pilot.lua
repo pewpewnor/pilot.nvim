e8fca6ccb973bdc2954ab191d53db065b67daa0f
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2026-06-27 03:54:52 +0800

    refactor: remove config from final exported module

--- lua/pilot.lua
+++ lua/pilot.lua
@@ -1,43 +1,3 @@
----@alias PilotFilepathResolver fun(): string?
-
----@alias Executor fun(command: string, args: string[]?)
-
----@class Executors
----@field [string] Executor
-
----@class Target
----@field pilot_file_path PilotFilepathResolver|PilotFilepathResolver[]
----@field auto_run_single_command boolean
----@field default_executor Executor
-
----@class Targets
----@field [string] Target
-
----@alias PlaceholderVar fun(): string
-
----@alias PlaceholderFunc fun(arg: string): string
-
----@class PlaceholderVars
----@field [string] PlaceholderVar
-
----@class PlaceholderFuncs
----@field [string] PlaceholderFunc
-
----@class Placeholders
----@field vars PlaceholderVars
----@field funcs PlaceholderFuncs
-
----@class Display
----@field numbered boolean
----@field last_entry_new_line boolean
-
----@class Config
----@field targets Targets
----@field write_template_to_new_pilot_file boolean
----@field executors Executors
----@field placeholders Placeholders
----@field display Display
-
 local default_config = require("pilot.default_config")
 local module = require("pilot.module")
 local validate_opts = require("pilot.validate_opts")
@@ -52,10 +12,10 @@ local M = {
 
 ---@param options table?
 function M.setup(options)
-    M.config =
+    config =
         vim.tbl_deep_extend("force", default_config.default_opts, options or {})
-    validate_opts(M.config)
-    module.init(M.config)
+    validate_opts(config)
+    module.init(config)
 end
 
 M.run_target = module.run_target
--- lua/pilot/default_config.lua
+++ lua/pilot/default_config.lua
@@ -1,3 +1,43 @@
+---@alias PilotFilepathResolver fun(): string?
+
+---@alias Executor fun(command: string, args: string[]?)
+
+---@class Executors
+---@field [string] Executor
+
+---@class Target
+---@field pilot_file_path PilotFilepathResolver|PilotFilepathResolver[]
+---@field auto_run_single_command boolean
+---@field default_executor Executor
+
+---@class Targets
+---@field [string] Target
+
+---@alias PlaceholderVar fun(): string
+
+---@alias PlaceholderFunc fun(arg: string): string
+
+---@class PlaceholderVars
+---@field [string] PlaceholderVar
+
+---@class PlaceholderFuncs
+---@field [string] PlaceholderFunc
+
+---@class Placeholders
+---@field vars PlaceholderVars
+---@field funcs PlaceholderFuncs
+
+---@class Display
+---@field numbered boolean
+---@field last_entry_new_line boolean
+
+---@class Config
+---@field targets Targets
+---@field write_template_to_new_pilot_file boolean
+---@field executors Executors
+---@field placeholders Placeholders
+---@field display Display
+
 local common = require("pilot.common")
 
 local M = {
